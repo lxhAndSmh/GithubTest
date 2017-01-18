c8bcb3ffbb189d342646017a9c338d3c894b4fac
lxhAndSmh <Lxh_smh5558@163.com>
2017-01-18 12:50:29 +0800

    合并branchLiu成功

--- app/src/main/java/com/liuxuhui/githuptest/SecondActivity.java
+++ app/src/main/java/com/liuxuhui/githuptest/SecondActivity.java
@@ -3,14 +3,11 @@ package com.liuxuhui.githuptest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //第一次修改master
-        //第二次修改了branchliu
-        //branchLiu_12:12
+        setContentView(R.layout.activity_second);
     }
 }
