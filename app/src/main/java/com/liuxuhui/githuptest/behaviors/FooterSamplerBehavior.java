a351ab0a5df7fb8adb20c41c451abe0bc4333495
lxhAndSmh <Lxh_smh5558@163.com>
2017-02-10 18:31:35 +0800

    Material Design之自定义Behavior

--- app/src/main/java/com/liuxuhui/githuptest/behaviors/FooterSamplerBehavior.java
+++ app/src/main/java/com/liuxuhui/githuptest/behaviors/FooterSamplerBehavior.java
@@ -1,14 +1,18 @@
 package com.liuxuhui.githuptest.behaviors;
 
+import android.animation.Animator;
 import android.content.Context;
-import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewPropertyAnimator;
+import android.view.animation.Interpolator;
 
 /**
  * 项目名称：GithupTest
- * 类描述：
+ * 类描述：自定义Behavior，底部随内容滑动显示或隐藏
  * 创建人：liuxuhui
  * 创建时间：2017/2/9 18:59
  * 修改人：liuxuhui
@@ -18,19 +22,84 @@ import android.view.View;
 
 public class FooterSamplerBehavior extends CoordinatorLayout.Behavior<View> {
 
+    private static final Interpolator INTERPOLATOR = new FastOutSlowInInterpolator();
+
+    private int sinceDirectionChange;
+
     public FooterSamplerBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
+    //判断滑动的方向 我们需要垂直滑动
     @Override
-    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
-        return dependency instanceof AppBarLayout;
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, View child, View directTargetChild, View target, int nestedScrollAxes) {
+        return (nestedScrollAxes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
     }
 
+    //根据滑动的距离显示和隐藏footer view
     @Override
-    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
-        float transY = Math.abs(dependency.getTranslationY());
-        child.setTranslationY(transY);
-        return true;
+    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dx, int dy, int[] consumed) {
+        if(dy > 0 && sinceDirectionChange < 0 || dy <0 && sinceDirectionChange > 0){
+            child.animate().cancel();
+            sinceDirectionChange = 0;
+        }
+        sinceDirectionChange += dy;
+        if(sinceDirectionChange > child.getHeight() && child.getVisibility() == View.VISIBLE){
+            hide(child);
+        }else if(sinceDirectionChange < 0 && child.getVisibility() == View.GONE){
+            show(child);
+        }
+    }
+
+    private void hide(final View view){
+        ViewPropertyAnimator animator = view.animate().translationY(view.getHeight()).setInterpolator(INTERPOLATOR).setDuration(200);
+        animator.setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                view.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                show(view);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        animator.start();
+    }
+
+    private void show(final View view){
+        ViewPropertyAnimator animator = view.animate().translationY(0).setInterpolator(INTERPOLATOR).setDuration(200);
+        animator.setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                view.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                hide(view);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        animator.start();
     }
 }
---
+++ app/src/main/java/com/liuxuhui/githuptest/behaviors/FooterSamplerBehaviorDependAppbar.java
@@ -0,0 +1,36 @@
+package com.liuxuhui.githuptest.behaviors;
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * 项目名称：GithupTest
+ * 类描述：自定义Behavior，仿简书滑动隐藏头部的底部，全屏效果
+ * 创建人：liuxuhui
+ * 创建时间：2017/2/10 11:15
+ * 修改人：liuxuhui
+ * 修改时间：2017/2/10 11:15
+ * 修改备注：
+ */
+
+public class FooterSamplerBehaviorDependAppbar extends CoordinatorLayout.Behavior<View> {
+
+    public FooterSamplerBehaviorDependAppbar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+        return dependency instanceof AppBarLayout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+        float transY = Math.abs(dependency.getTop());
+        child.setTranslationY(transY);
+        return true;
+    }
+}
